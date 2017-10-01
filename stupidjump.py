693d2abcdfa2af33fef3ec3342b321b980b04b6e
nuttapon <nuttapon.wac@ku.th>
2017-10-01 14:21:09 +0700

    stupid can jump by press spacebar

--- stupidjump.py
+++ stupidjump.py
@@ -1,13 +1,43 @@
 import arcade
+from stupidmodels import World,Stupid
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 675
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 class StupidWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
         arcade.set_background_color(arcade.color.BLACK)
+
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.stupid_sprite = ModelSprite('images/block.png',model=self.world.stupid)
+
+    def on_draw(self):
+        arcade.start_render()
+
+        self.stupid_sprite.draw()
+
+    def update(self, delta):
+        self.world.update(delta)
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
+
 
 def main():
     window = StupidWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
---
+++ stupidmodels.py
@@ -0,0 +1,40 @@
+import arcade.key
+
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.stupid = Stupid(self, 65, 100)
+
+
+    def update(self, delta):
+        self.stupid.update(delta)
+
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.SPACE:
+            self.stupid.switch_direction()
+
+class Stupid:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.vx = -15
+
+
+    def update(self, delta):
+        if self.x >= self.world.width-65:
+            pass
+        elif self.x > 65:
+            self.x+=self.vx
+
+
+        '''
+        if self.y > self.world.height:
+            self.y = 0
+        self.y += 5
+        '''
+    def switch_direction(self):
+        self.vx *= -1
+        self.x += self.vx
