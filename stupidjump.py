493b3593315168b6951a6590dc3006cdff8f1d0a
nuttapon <nuttapon.wac@ku.th>
2017-10-11 17:10:25 +0700

    big update..........

--- stupidjump.py
+++ stupidjump.py
@@ -1,9 +1,10 @@
 import arcade
-from stupidmodels import World,Stupid,Coin,Obstacle
+from stupidmodels import *
 
 
 SCREEN_WIDTH = 530
 SCREEN_HEIGHT = 725
+
 
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
@@ -23,30 +24,34 @@ class StupidWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.BLACK)
+        arcade.set_background_color(arcade.color.BLUE_GRAY)
 
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.stupid_sprite = ModelSprite('images/block.png',model=self.world.stupid)
+        self.stupid_sprite = ModelSprite('images/stupidchar.png',model=self.world.stupid)
         self.coin_sprite = ModelSprite('images/block.png',model=self.world.coin)
-        self.obstacleL_sprite = ModelSprite('images/heart.png',model=self.world.obstacleLeft)
-        self.obstacleR_sprite = ModelSprite('images/heart.png',model=self.world.obstacleRight)
-        self.endd = arcade.create_text("GAME OVER", arcade.color.WHITE, 20)
+        self.obstacleL_sprite = ModelSprite('images/fire.png',model=self.world.obstacleLeft)
+        self.obstacleR_sprite = ModelSprite('images/fire.png',model=self.world.obstacleRight)
+        self.endd = arcade.create_text("GAME OVER", arcade.color.BLACK, 20)
 
     def on_draw(self):
         arcade.start_render()
+        draw_background()
         if(self.world.endd != "GAME OVER"):
             self.stupid_sprite.draw()
             self.coin_sprite.draw()
             self.obstacleL_sprite.draw()
             self.obstacleR_sprite.draw()
+            for bs in self.world.bonus_list:
+                ModelSprite('images/heart.png',model=bs).draw()
+
             arcade.draw_text(str(self.world.score),
                          self.width - 30, self.height - 30,
-                         arcade.color.WHITE, 20)
+                         arcade.color.BLACK, 20)
         else:
             arcade.render_text(self.endd,self.width/3, self.height/2)
             arcade.draw_text("score = {}".format(str(self.world.score)),
                          self.width/2.6, self.height/3.1,
-                         arcade.color.WHITE, 20)
+                         arcade.color.BLACK, 20)
 
 
     def update(self, delta):
@@ -55,6 +60,10 @@ class StupidWindow(arcade.Window):
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
 
+def draw_background():
+    arcade.draw_lrtb_rectangle_filled(0,38,SCREEN_HEIGHT,0,arcade.color.BROWN_NOSE)
+
+    arcade.draw_lrtb_rectangle_filled(SCREEN_WIDTH-38,SCREEN_WIDTH,SCREEN_HEIGHT,0,arcade.color.BROWN_NOSE)
 
 def main():
     window = StupidWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- stupidmodels.py
+++ stupidmodels.py
@@ -1,5 +1,5 @@
 import arcade.key
-from random import randint
+from random import *
 
 
 class World:
@@ -8,6 +8,8 @@ class World:
         self.height = height
         self.score = 0
         self.endd = ""
+        self.time = 0
+
 
 
         self.stupid = Stupid(self, 65, 100)
@@ -15,7 +17,7 @@ class World:
 
         self.obstacleLeft = Obstacle(self,65,height,randint(2,10))
         self.obstacleRight = Obstacle(self,width-65,height,randint(2,10))
-
+        self.bonus_list = []
 
 
     def update(self, delta):
@@ -24,12 +26,28 @@ class World:
         self.obstacleRight.update(delta)
         self.obstacleLeft.update(delta)
 
+        self.time+=delta
+        if(self.time>randint(2,5)):
+            self.bonus_list.append(Bonus(self,randint(100,self.width-100),self.height,randint(5,15)))
+            self.time = 0
 
+        for index,b in enumerate(self.bonus_list):
+            b.update(delta)
+            if self.stupid.hit(b,35):
+                self.score+=5
+                del self.bonus_list[index]
+                
+            if(b.is_out_of_screen()):
+                del self.bonus_list[index]
+
+        print(self.bonus_list)
         if self.stupid.hit(self.coin, 25):
             self.coin.random_location()
             self.score+=1
-        if self.stupid.hit(self.obstacleLeft, 5) or self.stupid.hit(self.obstacleRight, 5):
+        if self.stupid.hit(self.obstacleLeft, 35) or self.stupid.hit(self.obstacleRight, 35):
             self.endd = "GAME OVER"
+
+
 
     def on_key_press(self, key, key_modifiers):
         if self.stupid.x ==65 or self.stupid.x == self.width-65:
@@ -94,5 +112,27 @@ class Obstacle:
     def update(self, delta):
         if self.y <0:
             self.y = self.world.height
-
+            self.vy = randint(2,10)
         self.y-=self.vy
+
+class Bonus:
+    def __init__(self, world, x, y, vy):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.vy = vy
+
+    def __repr__(self):
+        return "Bonus[{},{}]->{}".format(self.x,self.y,self.vy)
+
+
+    def is_out_of_screen(self):
+        if self.y < 0:
+            return True
+        return False
+
+    def movement(self):
+        self.y-=self.vy
+
+    def update(self, delta):
+        self.movement()
